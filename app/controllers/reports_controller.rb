d3877013afde075e1a15ecca21313d7a89cfd00d
Artem Kulakov <artem.kulakov@gmail.com>
2019-12-07 18:56:07 +0300

    Filter tickets for report by dates

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,20 @@ class ReportsController < ApplicationController
         redirect_to new_reports_path
       end
       format.csv do
-        @tickets = Ticket.all.last(10)
+        start_date = params[:start_date].empty? ? '1900-01-01' : params[:start_date]
+        end_date = params[:end_date].empty? ? DateTime.now.strftime("%Y-%m-%d %H:%M:%S") : params[:end_date] + ' 23:59:59'
+
+        if params[:end_date].empty?
+          @tickets = Ticket
+            .by_label_id(params[:label])
+            .where("tickets.created_at >= :start_date AND tickets.created_at <= :end_date", {start_date: start_date, end_date: end_date})
+        else
+          @tickets = Ticket
+            .by_label_id(params[:label])
+            .by_status(:closed)
+            .joins(:status_changes)
+            .where("tickets.created_at >= :start_date AND status_changes.updated_at <= :end_date AND status_changes.status = :status", {start_date: start_date, end_date: end_date, status: 1})
+        end
       end
     end
   end
