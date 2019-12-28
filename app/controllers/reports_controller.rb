962b7dba197fe9bdda2b9a49422d8fc3e1664784
Artem Kulakov <artem.kulakov@gmail.com>
2019-12-28 21:14:54 +0300

    Add tickets with all statuses (open, deleted etc) to CSV report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,17 +14,9 @@ class ReportsController < ApplicationController
         start_date = params[:start_date].empty? ? '1900-01-01' : params[:start_date]
         end_date = params[:end_date].empty? ? DateTime.now.strftime("%Y-%m-%d %H:%M:%S") : params[:end_date] + ' 23:59:59'
 
-        if params[:end_date].empty?
-          @tickets = Ticket
-            .by_label_id(params[:label])
-            .where("tickets.created_at >= :start_date AND tickets.created_at <= :end_date", {start_date: start_date, end_date: end_date})
-        else
-          @tickets = Ticket
-            .by_label_id(params[:label])
-            .by_status(:closed)
-            .joins(:status_changes)
-            .where("tickets.created_at >= :start_date AND status_changes.updated_at <= :end_date AND status_changes.status = :status", {start_date: start_date, end_date: end_date, status: 1})
-        end
+        @tickets = Ticket
+          .by_label_id(params[:label])
+          .where("tickets.created_at >= :start_date AND tickets.created_at <= :end_date", {start_date: start_date, end_date: end_date})
       end
     end
   end
